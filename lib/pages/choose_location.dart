dd595ab8a32422b10b29a5e58f965bbf7c5a05d6
Kizomba <kizomba@github.com>
2021-03-16 20:53:03 +0100

    http get does not work

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,25 +9,6 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   //int counter = 0;
 
-  void getData() async {
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      //print('yyy1');
-      return 'yyy1';
-    });
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      //print('yyy2');
-      return 'yyy2';
-    });
-    print('yyy3 - $username - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('ttt1');
-  }
-
   @override
   Widget build(BuildContext context) {
     print('bbb ttt1');
